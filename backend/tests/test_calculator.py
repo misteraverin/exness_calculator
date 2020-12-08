93b2a2e91f79efb469421f692bb98eb5a134f4f7
Maxim Averin <amax0703@gmail.com>
2020-12-08 15:08:11 +0300

    Reformat code with black

--- backend/tests/test_calculator.py
+++ backend/tests/test_calculator.py
@@ -5,14 +5,14 @@ from app.calculator import Calculator
 
 
 @pytest.mark.parametrize(
-    'test_value,expected_result',
+    "test_value,expected_result",
     [
-        (('UK', 12, 13), Decimal('156')),
-        (('UT', 100, 13), Decimal('1347.3785')),
-        (('NV', 6000, 1), Decimal('6156')),
-        (('CA', 7456, 1), Decimal('7506.1416')),
-        (('AL', 13000, 3), Decimal('36504')),
-        (('TX', 50000, 10), Decimal('451562.5')),
+        (("UK", 12, 13), Decimal("156")),
+        (("UT", 100, 13), Decimal("1347.3785")),
+        (("NV", 6000, 1), Decimal("6156")),
+        (("CA", 7456, 1), Decimal("7506.1416")),
+        (("AL", 13000, 3), Decimal("36504")),
+        (("TX", 50000, 10), Decimal("451562.5")),
     ],
 )
 def test_calculate_total_order_price(test_value, expected_result):
--- backend/tests/test_server.py
+++ backend/tests/test_server.py
@@ -15,27 +15,33 @@ async def test_get_index_page():
 @pytest.mark.asyncio
 async def test_calculate_total_order_price():
     async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.post("/calculate/", data={
-            'product_quantity': 12,
-            'price': 13,
-            'state_code': 'UT',
-        })
+        response = await ac.post(
+            "/calculate/",
+            data={
+                "product_quantity": 12,
+                "price": 13,
+                "state_code": "UT",
+            },
+        )
     assert response.status_code == 200
-    soup = BeautifulSoup(response.text, 'html.parser')
-    result = soup.find(id='total_order_price')
+    soup = BeautifulSoup(response.text, "html.parser")
+    result = soup.find(id="total_order_price")
     assert "Total order price: 166.6860" == result.get_text()
 
 
 @pytest.mark.asyncio
 async def test_calculate_total_order_price_with_wrong_params():
     async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.post("/calculate/", data={
-            'product_quantity': 12,
-            'price': 13,
-            'state_code': 'UK',
-        })
+        response = await ac.post(
+            "/calculate/",
+            data={
+                "product_quantity": 12,
+                "price": 13,
+                "state_code": "UK",
+            },
+        )
     assert response.status_code == 200
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, "html.parser")
     all_errors = soup.findAll("li", {"class": "error"})
     assert 1 == len(all_errors)
-    assert all_errors[0].get_text() == 'write correct US state'
+    assert all_errors[0].get_text() == "write correct US state"
